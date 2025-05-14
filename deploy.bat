6f14ff83ade8064464d45e11c3375a935a73a4b4
Josue Henrique <josue21henrique@gmail.com>
2025-05-14 10:20:24 -0300

    update deploy

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,3 @@
 @echo off
 echo Conectando-se ao servidor...
-ssh agendoaqui@agendoaqui.infotech.app.br "cd htdocs && cd agendoaqui.infotech.app.br && git pull && npm install && npm run build"
+ssh agendoaqui@agendoaqui.infotech.app.br "cd htdocs && cd agendoaqui.infotech.app.br && git pull && npm install --force && npm run build"
